1b36c4c52ad5f233c48c4c9853d84347630a272b
Tony-Kara <eniolaanthony@gmail.com>
2021-09-22 19:17:11 +0600

    - Add GeometryReader, Tabview - Prepare the basics of the UI

--- UIChallenge/ContentView.swift
+++ UIChallenge/ContentView.swift
@@ -9,8 +9,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+
+        
+     
+           
+       
+            
+        VStack(alignment: .leading, spacing: 0) {
+            
+            GeometryReader { geo in
+                
+                    TabView {
+                    
+                        ZStack {
+                            
+                            Rectangle()
+                        
+                        }
+                        .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                            .cornerRadius(15)
+                                .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+            }
+            }
+        }
+        
+                
+              
+       
     }
 }
 
