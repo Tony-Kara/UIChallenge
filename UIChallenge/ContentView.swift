928f02327d2159228f4cc5e507741782f14aa2df
Tony-Kara <eniolaanthony@gmail.com>
2021-09-22 19:50:24 +0600

    -Create property to return random number color - Use ForEach to create a loop of numbers from 1 to 50 - Add system image and Pass index from Foreach to the system image name to switch  numbers from one to 50 - Add a computed property that returns various colors randomly and pass it to the rectangle() frame to change the foreground color

--- UIChallenge/ContentView.swift
+++ UIChallenge/ContentView.swift
@@ -8,8 +8,21 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    var cardColor: Color {
+        return Color(
+            .sRGB,
+            red: Double.random(in: 0..<1),
+            green: Double.random(in: 0..<1),
+            blue: Double.random(in: 0..<1),
+            opacity: 1
+        )
+    }
+    
+    
     var body: some View {
 
+        
         
      
            
@@ -21,15 +34,33 @@ struct ContentView: View {
                 
                     TabView {
                     
-                        ZStack {
+                        ForEach(0..<51){ index  in
+                            ZStack {
+                                
+                                Rectangle()
+                                    .foregroundColor(cardColor)
+                                
+                                VStack {
+                                    
+                                    Image(systemName: "\(index).circle")
+                                        .resizable()
+                                        .aspectRatio(contentMode: .fit)
+                                        .padding()
+                                }
+                                    
                             
-                            Rectangle()
-                        
+                            }
+                            .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
+                           
+                                .cornerRadius(20)
+                                    .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
                         }
-                        .frame(width: geo.size.width - 40, height: geo.size.height - 100, alignment: .center)
-                            .cornerRadius(15)
-                                .shadow(color: Color(.sRGB, red: 0, green: 0, blue: 0, opacity: 0.5), radius: 10, x: -5, y: 5)
+                        
+                       
             }
+                    .tabViewStyle(PageTabViewStyle(indexDisplayMode: .automatic))
+                    //this modifier ensures that the ... for scrolling pages always appears
+                    .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .always))
             }
         }
         
